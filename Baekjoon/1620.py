c990742b1369d6dbee42046dc04aa652b2d26d4a
jangseoyoung98 <77472570+jangseoyoung98@users.noreply.github.com>
2023-03-13 15:10:21 +0900

    1620 3rd try(time over)

--- Baekjoon/1620.py
+++ Baekjoon/1620.py
@@ -1,42 +1,37 @@
 #1. 사용자로부터 N과 M을 입력 받아 변수에 저장한다.
 n, m = map(int, input().split(' '))
-dogam = []
+dogamId = [-1] # 인덱스 1부터 채우기 위해 -1을 임시 할당함 (의미 X)
+dogamName = ['NA'] # 인덱스 1부터 채우기 위해 NA를 임시 할당함 (의미 X)
 answer = ""
 
-#2. N번 만큼 돌면서, 포켓몬의 이름을 입력 받아 각 포켓몬을 딕셔너리 형태로 리스트에 저장한다.
+#2. N번 만큼 돌면서, 포켓몬의 이름을 입력 받아 할당 id와 이름을 각각 dogamName과 dogamID에 추가한다.
 for i in range(1, n + 1):
-    pocketmon = input()
+    poketmon = input()
 
     # (조건) 포켓몬의 이름은 2~20자 + 첫 글자 혹은 마지막 글자만 대문자를 제한으로 한다.
-    if len(pocketmon) < 2 or len(pocketmon) > 20:
+    if len(poketmon) < 2 or len(poketmon) > 20:
         print("이름 다시 입력!\n")
         i -= 1
         continue
-    elif not pocketmon[1:len(pocketmon)-2].islower():
+    elif not poketmon[1:len(poketmon)-2].islower():
         print("이름 다시 입력!\n")
         i -= 1
         continue
 
-    dogam.append({'num': i, 'name':pocketmon})
+    dogamId.append(i) # 1번부터!
+    dogamName.append(poketmon)
 
 #3. M번 만큼 돌면서, 문제를 입력 받고, 해당 문제의 결과를 찾아 리스트로 저장한다.
 for i in range(m):
     qna = input()
-    # 알파벳 입력 받으면 -> 번호
-    if(ord(qna[0:1]) >= 65 and ord(qna[0:1]) <= 122):
-        # 해당 포켓몬의 번호를 찾아서
-        # index = dogam.index({'name':qna})
-        ret = next((item for item in dogam if item['name'] == qna), None)
-        # answer에 넣는다.
-        answer += f"{ret['num']}\n"
-
     # 숫자를 입력 받으면 -> 이름
+    if qna.isdigit():
+        answer += f"{dogamName[int(qna)]}\n"
+
+    # 알파벳 입력 받으면 -> 번호
     else:
-        # 해당 포켓몬의 이름을 찾아서
-        # index = dogam.index({'num':int(qna)})
-        ret = next((item for item in dogam if item['num'] == int(qna)), None)
-        # answer에 넣는다.
-        answer += f"{ret['name']}\n"
+        num = dogamName.index(qna)
+        answer += f"{dogamId[num]}\n"
     
 #4. answer을 한 번에 출력한다.
 print(answer)
