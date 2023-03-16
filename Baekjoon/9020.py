55bae913c68b5b914e5f5b9b27bd9dedcaec3da7
jangseoyoung98 <77472570+jangseoyoung98@users.noreply.github.com>
2023-03-16 17:09:35 +0900

    9020 not yet

--- Baekjoon/9020.py
+++ Baekjoon/9020.py
@@ -1,36 +1,51 @@
-# 백준 9020번: 골드바흐의 추측
-# Algorithm to 입력으로 들어온 짝수를 골드바흐의 파티션으로 출력한다.
-# input : 테스트 케이스 갯수 T / 짝수인 정수 n
-# output : 정수 n에 대한 골드바흐의 파티션 -> 소수는 작은 것부터, 여러 파티션 중에서는 둘 차이가 작은 것
-
-# 0. T를 변수에 담아, T만큼 for문을 돌린다.
+#1. 갯수 T를 입력 받는다.
 T = int(input())
-n = int(input())
-for i in range(T):
-    # 1. n/2가 짝수라면 - n/2-1과 n/2+1
-    #    n/2가 홀수라면 - n/2와 n/2
-    if n % 2 == 0 : 
-        left = n // 2 - 1
-        right = n // 2 + 1
-    else:
-        left = right = n // 2
+nList = []
+answer= ""
+
+#2. T만큼 돌리면서 n을 입력 받는다. (n1, n2, ... nT)
+for _ in range(T):
+    n = int(input())
+    nList.append(n)
+
+#3. 리스트에 들어간 n들에 대해 약수&소수 판별한다.
+# 1) 입력 받은 n의 약수를 구한다. -> n/2부터 시작해서, 양쪽으로 퍼지기 
+# -> 여기서 for문을 돌리면 안 될 거 같다..
+# -> 번갈아서 /2 한 약수가 짝수-짝수 / 홀수-홀수로 나뉜다.
+## -> 짝수-짝수인 경우 바로 건너 뛰기
+
+for n in nList:
+    temp = n // 2
+    isPrime = False
     
-    # 2. 양 끝 단이 소수인지 검사해서 맞으면 바로 출력하고 / 아니면 -2와 +2씩 한다. (1보다 크고, n보다 작을 때까지)
-    while(left >= 1 and right < n):
-        isDec = 1
-        for i in range(2, left // 2 + 1):
-            if left % i == 0:
-                isDec = 0
-                break
-            if right % i == 0:
-                isDec = 0
-                break
-        
-        if(isDec):
-            print(left, right)
+    if n % 2 == 0: # 짝수라면 -> prime1과 2는 동일한 값
+        # prime1, prime2 = temp, temp -> 가능..?
+        prime1 = temp
+        prime2 = temp
+    else: # 홀수라면 -> prime1은 그대로, 2는 +1 한 값
+        prime1 = temp
+        prime2 = temp + 1   
+
+for i in (temp, 2, -2):
+    # 2) 그 약수가 소수인지 판단한다.
+    # -> 변수 true/false 조건을 걸어 다룬다.
+    prime1 = n - i
+    prime2 = i
+    for j in range(2, temp-1):
+        if temp % j == 0:
             break
-        
-        left -= 2
-        right += 2
+        else:
+            isPrime = True
+    # 3) 발견한 소수를 문자열에 저장한다.
+    if isPrime == True:
+        answer += f"{prime1} {prime2}\n"
+
+
+
+
+#3. 다 끝난 문자열을 출력한다.
+print(answer)
+
+
 
 
